686f61060ff0c485317a82406b070644aeacb7c2
Дамбийням У <dambii5655@gmail.com>
2023-03-10 09:10:11 +0800

    inp

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -1,3 +1,26 @@
 export default function Add() {
-  return <div>add</div>;
+  return (
+    <div>
+      <div>
+        <input type="text" placeholder="Name" />
+        <input type="text" placeholder="Price" />
+        <input type="text" placeholder="Image" />
+        <input type="text" placeholder="Brand" />
+      </div>
+      <div>
+        <input type="text" placeholder="Category" />
+        <button>Add+</button>
+      </div>
+      <h2>Add Categoty</h2>
+      <div>
+        <input type="text" placeholder="Category" />
+        <button>Add Category+</button>
+      </div>
+      <h2>Add Brand</h2>
+      <div>
+        <input type="text" placeholder="Brand" />
+        <button>Add Brand+</button>
+      </div>
+    </div>
+  );
 }
